fdded6a24151ce061a44c46ed17061253fef517e
Jinho <john3176@hotmail.com>
2024-11-07 07:34:11 +0900

    inflearn Java 3nd lecture

---
+++ 3rd_lecture/src/variable/Var8.java
@@ -0,0 +1,13 @@
+package variable;
+
+public class Var8 {
+    //정수
+    byte b = 127; //-128 ~ 127
+    short s = 32767; //-32,768 ~ 32767
+    int i = 2137483647; // +- 약 20억 까지
+    long l = 9223372036854775807L;
+
+    //실수
+    float f = 10.f; //Float은 뒤에 f를 붙여야함
+    double d = 10.0; //float보다 더 큰 수 저장 가능
+}
---
+++ 3rd_lecture/src/variable/ex/VarEx1Question.java
@@ -0,0 +1,15 @@
+package variable.ex;
+
+public class VarEx1Question {
+    public static void main(String[] args) {
+        System.out.println(4 + 3);
+        System.out.println(4 - 3);
+        System.out.println(4 * 3);
+
+        //
+        int num1 = 4, num2 = 3;
+        System.out.println(num1 + num2);
+        System.out.println(num1 - num2);
+        System.out.println(num1 * num2);
+    }
+}
